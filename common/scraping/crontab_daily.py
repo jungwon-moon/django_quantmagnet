303070db6350160beea8dd54f5f5d7dca199edac
jungwon-moon <jwoung0@naver.com>
2022-02-25 07:47:40 +0900

    슬랙 url 숨김처리, db 및 qm 변경에 따른 코드 수정

--- common/scraping/crontab_daily.py
+++ common/scraping/crontab_daily.py
@@ -2,22 +2,26 @@ import os
 from pathlib import Path
 import json
 import requests
-from qm import db, scraping
+from qm.db.connect import POSTGRESCRUD
+from qm import scraping
+
 
 ### slack webhook connect
-slack_url = 'https://hooks.slack.com/services/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'
 headers = {
     "Content-type": "application/json"
 }
 
-### mongodb connect
+### postgresql connect
 SECRET_PATH = Path(__file__).resolve().parent.parent.parent
 SECRET_FILE = SECRET_PATH / 'config/.config_secret/db.json'
-
 secrets = json.loads(open(SECRET_FILE).read())
+
 for key, value in secrets.items():
-    if key == 'host':
-        host = value
+    if key == 'gcp':
+        pgdb_properties = value
+    if key == 'slack_scraping':
+        slack_url = value
+
 
 ### functions
 def non_traiding_days():
@@ -25,22 +29,50 @@ def non_traiding_days():
     휴장일 갱신을 위해 매일 update 수행
     '''
     try:
-        data = scraping.get_non_trading_days()
-        mongodb = db.connect.mongodb_connect(host).qmdb
-        for row in data:
-            doc = {
-                'calnd_dd': ''.join(row['calnd_dd'].split('-')),
-                'dy_tp_cd': row['dy_tp_cd'],
-                'kr_dy_tp': row['kr_dy_tp'],
-                'holdy_nm': row['holdy_nm'],
-            }
-            mongodb.non_trading_days.update_one(doc, {"$set": doc}, upsert=True)
+        data = scraping.get_non_trading_days(Type='db')
+        db = POSTGRESCRUD(pgdb_properties)
 
-        txt = f'Run: non_traiding_days'
+        for row in data:
+            date = ''.join(row['calnd_dd'].split('-'))
+            value = date, row['dy_tp_cd'], row['kr_dy_tp'], row['holdy_nm']
+
+            db.insertDB('non_trading_days', '', value)
+
+        txt = f'| non_traiding_days | Run'
+        txt = json.dumps({"text": txt})
+        requests.post(slack_url, headers=headers, data=txt)
+
+    except Exception as e:
+        txt = f'| non_traiding_days | * Error * : {e}'
+        txt = json.dumps({"text": txt})
+        requests.post(slack_url, headers=headers, data=txt)
+
+
+def fundamental_v1():
+    try:
+        data = scraping.get_fundamental(Type='db')
+        # mongodb = db.connect.mongodb_connect(host).kr
+
+        for stock in data:
+            doc = {
+                'STCD': stock['ISU_SRT_CD'],
+                'STNM': stock['ISU_ABBRV'],
+                'EPS': stock['EPS'],
+                'PER': stock['PER'],
+                'FWD_EPS': stock['FWD_EPS'],
+                'FWD_PER': stock['FWD_PER'],
+                'BPS': stock['BPS'],
+                'PBR': stock['PBR'],
+                'DPS': stock['DPS'],
+                'DVD_YLD': stock['DVD_YLD']
+            }
+            # mongodb.stock_fundamental.insert_one(doc)
+
+        txt = f'Run: fundamental_v1'
         txt = json.dumps({"text": txt})
         requests.post(slack_url, headers=headers, data=txt)
 
     except:
-        txt = f'*** ERROR ***: non_traiding_days'
+        txt = f'*** ERROR ***: fundamental_v1'
         txt = json.dumps({"text": txt})
         requests.post(slack_url, headers=headers, data=txt)
