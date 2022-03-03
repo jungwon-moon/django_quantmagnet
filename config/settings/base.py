38b084f0db2efbea1b1d2fe14218bd1c07e1bce8
jungwon-moon <jwoung0@naver.com>
2022-03-03 22:13:31 +0900

    admin, rest_framwork, debug_toolbar static files

--- config/settings/base.py
+++ config/settings/base.py
@@ -152,8 +152,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'dev_static',
 ]
 
 # Default primary key field type
