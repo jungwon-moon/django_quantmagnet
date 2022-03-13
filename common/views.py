7d55ddcb0f014fe54b4003c37b2b0352453c3630
jungwon-moon <jwoung0@naver.com>
2022-03-13 23:57:15 +0900

    index -> main 명칭 수정

--- common/views.py
+++ common/views.py
@@ -1,3 +1,7 @@
+import json
+from qm import scraping
+from bokeh.plotting import figure
+from bokeh.embed import components
 from datetime import datetime
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, get_object_or_404, redirect
@@ -7,33 +11,22 @@ from common.forms import UserForm
 # from py import prac_bokeh
 # from qmscraping import krx, utils
 
+from math import pi
+import pandas as pd 
 
-def test(request):
-    """
-    test 페이지
-    """
-    # now = datetime.today()
-    # now = now.strftime('%Y-%m-%d')
-    # now = utils.check_trading_day(now)
-    # js = krx.fundamental_json(now)
-
-    return render(request, 'test2.html')
-    # return render(request, 'test copy.html', {'js': js, 'now': now})
-    # return render(request, 'test.html', {'s1': script1, 'd1': div1})
-
-
-def index(request):
+def main(request):
     """
     첫 페이지
     """
-    return render(request, 'index.html')
+    return render(request, 'common/main.html')
+
 
 def register(request):
     """
     계정생성
     """
     if request.user.is_authenticated:
-        return redirect('index')
+        return redirect('main')
 
     if request.method == "POST":
         form  = UserForm(request.POST)
@@ -43,14 +36,9 @@ def register(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('index')
+            return redirect('main')
     else:
         form = UserForm()
         
     return render(request, 'common/register.html', {'form': form})
 
-
-# def krxx(request):
-#     dddd = krx.ipo()
-
-#     return render(request, 'common/krx.html', {'dddd': dddd})
