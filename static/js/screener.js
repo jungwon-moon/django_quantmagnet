a14bbef5ba4fdd5f593169a61063df546894f105
jungwon-moon <jwoung0@naver.com>
2022-03-17 07:02:14 +0900

    종목 스크리너

--- static/js/screener.js
+++ static/js/screener.js
@@ -11,76 +11,128 @@ function inputNumberFormat(obj) {
     obj.value = comma(uncomma(obj.value));
 }
 
-for (const i in filtersList) {
-    var filterName = filtersList[i] // eps, per, bps, pbr, dps, dvd_yld    
-    var filters = $(`[data-id^=${filterName}]`) // objects
-    
-    const setLeftSlider = e => {
-        const _this = e.target;
-        const { value, min, max } = _this;
-        if (filters[3].value - +value < 0.1) {
-            _this.value = +filters[3].value - 0.1;
-        }
-        const percent = ((+_this.value - +min) / (+max - +min)) * 100;
-        filters[4].style.left = `${percent}%`;
-        filters[5].style.left = `${percent * .95}%`;
-        filters[7].value = _this.value;
-    };
-    
-    const setRightSlider = e => {
-        const _this = e.target;
-        const { value, min, max } = _this;
-        if (+value - +filters[2].value < 0.1) {
-            _this.value = +filters[2].value + 0.1;
-        }
-        const percent = ((+_this.value - +min) / (+max - +min)) * 100;
-        filters[4].style.right = `${100 - percent}%`;
-        filters[6].style.right = `${(100 - percent) * .95}%`;
-        filters[8].value = _this.value;
-    };
-    
-    const setLeftBox = e => {
-        const _this = Number(e.target.value);
-        const { min, max } = filters[2]
-        if ((_this >= min) && (_this < filters[3].value)) {
-            const percent = ((+_this - +min) / (+max - +min)) * 100;
-            filters[2].value = percent;
-            filters[4].style.left = `${percent}%`;
-            filters[5].style.left = `${percent *.95}%`;
-        }
-        else {
+var filterList = document.querySelectorAll("div.filter")
 
-        }
+filterList.forEach(filter => {
+    var filterName = filter.querySelector(".filter-name")
+    var inputLeft = filter.querySelector(".left")
+    var inputRight = filter.querySelector("input.right")
+    var textLeft = filter.querySelector(".text.left")
+    var textRight = filter.querySelector(".text.right")
+
+    filterName.addEventListener("mouseover", e => {
+        filterName.style.backgroundColor = "white";
+    }, false);
+    filterName.addEventListener("mouseout", function (e) {
+        filterName.style.backgroundColor = "#e9ecef";
+    }, false);
+    filterName.addEventListener("click", filterNameClick, false);
+    inputLeft.addEventListener("input", setLeftSlider, false)
+    inputRight.addEventListener("input", setRightSlider, false)
+    textLeft.addEventListener("change", setLeftBox, false)
+    textRight.addEventListener("change", setRightBox, false)
+})
+
+
+function filterNameClick() {
+    var filterMain = this.parentNode.querySelector(".filter-main")
+    if (filterMain.style.display == "none") {
+        filterMain.style.display = "block"
     }
-
-    const setRightBox = e => {
-        const _this = Number(e.target.value);
-        const { min, max } = filters[3]
-        if ((_this <= max) && (_this > filters[2].value)) {
-            const percent = ((+_this - +min) / (+max - +min)) * 100;
-            console.log(comma(percent))
-            filters[3].value = percent;
-            filters[4].style.right = `${100 - percent}%`;
-            filters[6].style.right = `${(100 - percent) *.95}%`;
-        }
+    else {
+        filterMain.style.display = "none"
     }
-
-    // slider 동작 및 input text-box 값 수정
-    if (filters[2] && filters[3]) {
-        filters[2].addEventListener("input", setLeftSlider);
-        filters[3].addEventListener("input", setRightSlider);
-    };
-    
-    // input text-box 값 수정 시 slider 동작
-    if (filters[7] && filters[8]) {
-        filters[7].addEventListener("change", setLeftBox)
-        filters[8].addEventListener("change", setRightBox)
-    };
 };
+
+function setLeftSlider() {
+    var parent = this.parentNode.parentNode;
+    var range = parent.querySelector('div.range');
+    var inputRight = parent.querySelector('input.right');
+    var thumbLeft = parent.querySelector('.thumb.left');
+    var textLeft = parent.querySelector('.text.left');
+
+    const { value, min, max } = this;
+    if (inputRight.value - value < 0.1) {
+        this.value = inputRight.value - 0.1;
+    }
+    const percent = ((this.value - min) / (max - min)) * 100;
+    range.style.left = `${percent}%`;
+    thumbLeft.style.left = `${percent * .95}%`;
+    textLeft.value = this.value;
+};
+
+function setRightSlider() {
+    var parent = this.parentNode.parentNode;
+    var range = parent.querySelector('div.range');
+    var inputLeft = parent.querySelector('input.left');
+    var thumbRight = parent.querySelector('.thumb.right');
+    var textRight = parent.querySelector('.text.right');
+
+    const { value, min, max } = this;
+    if (value - inputLeft.value < 0.1) {
+        this.value = inputLeft.value + 0.1;
+    }
+    const percent = ((this.value - min) / (max - min)) * 100;
+    range.style.right = `${100 - percent}%`;
+    thumbRight.style.right = `${(100 - percent) * .95}%`;
+    textRight.value = this.value;
+};
+
+function setLeftBox() {
+    var parent = this.parentNode.parentNode;
+    var range = parent.querySelector('div.range');
+    var inputLeft = parent.querySelector('input.left');
+    var inputRight = parent.querySelector('input.right');
+    var thumbLeft = parent.querySelector('.thumb.left');
+    var textLeft = parent.querySelector('.text.left');
+
+    var { min, max } = inputLeft;
+    const value = Number(uncomma(textLeft.value));
+
+    if ((value >= min) && (value < inputRight.value)) {
+        const percent = ((value - min) / (max - min)) * 100;
+        inputLeft.value = value;
+        range.style.left = `${percent}%`;
+        thumbLeft.style.left = `${percent * .95}%`;
+    }
+    else {
+        textLeft.value = min;
+        inputLeft.value = min;
+        range.style.left = `0%`;
+        thumbLeft.style.left = `0%`;
+    }
+};
+
+function setRightBox() {
+    var parent = this.parentNode.parentNode;
+    var range = parent.querySelector('div.range');
+    var inputLeft = parent.querySelector('input.left');
+    var inputRight = parent.querySelector('input.right');
+    var thumbRight = parent.querySelector('.thumb.right');
+    var textRight = parent.querySelector('.text.right');
+
+    var { min, max } = inputRight;
+    const value = Number(uncomma(textRight.value));
+
+    if ((value <= max) && (value > inputLeft.value)) {
+        const percent = ((value - min) / (max - min)) * 100;
+        inputRight.value = value;
+        range.style.right = `${100 - percent}%`;
+        thumbRight.style.right = `${(100 - percent) * .95}%`;
+    }
+    else {
+        textRight.value = max;
+        inputRight.value = max;
+        range.style.right = `0%`;
+        thumbRight.style.right = `0%`;
+    }
+};
+
 
 // resultTable 출력 함수
 function setTable(result) {
     const table = document.getElementById("tableOutput").getElementsByTagName('tbody')[0];
+    // 테이블 초기화
     table.innerHTML = '';
     for (let i = 1; i < result.length; i++) {
         var row = `
