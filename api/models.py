cff0d751a9f748d9d337441292f371f763eea71e
jungwon-moon <jwoung0@naver.com>
2022-05-31 20:45:40 +0900

    종목 검색 API 수정

--- api/models.py
+++ api/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
+
 # # STOCK
-
-
 class Valuation(models.Model):
     date = models.CharField(max_length=8)
     stcd = models.CharField(max_length=6)
@@ -55,7 +54,7 @@ class StockPrice(models.Model):
 
 class Stocks(models.Model):
     stcd = models.CharField(max_length=6)
-    stnm = models.TextField(blank=True, null=True)
+    stnm = models.TextField(primary_key=True)
     market = models.TextField(blank=True, null=True)
 
     class Meta:
