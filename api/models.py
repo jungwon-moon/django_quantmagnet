d380aa59390c4b080acdf6295e7ca346ada34385
jungwon-moon <jwoung0@naver.com>
2023-08-15 00:24:53 +0900

    거래대금 상승종목 db model 작성 + 주석 작성

--- api/models.py
+++ api/models.py
@@ -2,7 +2,48 @@ from django.db import models
 
 
 # # STOCK
+class Holiday(models.Model):
+    """
+    휴장일 정보
+    """
+    calnd_dd = models.CharField(primary_key=True, max_length=8)
+    dy_tp_cd = models.CharField(max_length=3, blank=True, null=True)
+    kr_dy_tp = models.CharField(max_length=3, blank=True, null=True)
+    holdy_nm = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'holiday'
+        verbose_name_plural = "휴장일"
+
+
+class StockPrice(models.Model):
+    """
+    주가 
+    """
+    date = models.CharField(max_length=8, primary_key=True)
+    stcd = models.CharField(max_length=6)
+    market = models.TextField(blank=True, null=True)
+    rate = models.FloatField(blank=True, null=True)
+    prevd = models.FloatField(blank=True, null=True)
+    open = models.FloatField(blank=True, null=True)
+    high = models.FloatField(blank=True, null=True)
+    low = models.FloatField(blank=True, null=True)
+    close = models.FloatField(blank=True, null=True)
+    volume = models.FloatField(blank=True, null=True)
+    value = models.FloatField(blank=True, null=True)
+    capital = models.FloatField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'stock_price'
+        unique_together = (('date', 'stcd'),)
+
+
 class Valuation(models.Model):
+    """
+    가치평가(밸류에이션)
+    """
     date = models.CharField(max_length=8, primary_key=True)
     stcd = models.CharField(max_length=6)
     stnm = models.TextField(blank=True, null=True)
@@ -20,39 +61,10 @@ class Valuation(models.Model):
         unique_together = (('date', 'stcd'),)
 
 
-class Holiday(models.Model):
-    calnd_dd = models.CharField(primary_key=True, max_length=8)
-    dy_tp_cd = models.CharField(max_length=3, blank=True, null=True)
-    kr_dy_tp = models.CharField(max_length=3, blank=True, null=True)
-    holdy_nm = models.TextField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'holiday'
-        verbose_name_plural = "휴장일"
-
-
-class StockPrice(models.Model):
-    date = models.CharField(max_length=8, primary_key=True)
-    stcd = models.CharField(max_length=6)
-    market = models.TextField(blank=True, null=True)
-    rate = models.FloatField(blank=True, null=True)
-    prevd = models.FloatField(blank=True, null=True)
-    open = models.FloatField(blank=True, null=True)
-    high = models.FloatField(blank=True, null=True)
-    low = models.FloatField(blank=True, null=True)
-    close = models.FloatField(blank=True, null=True)
-    volume = models.FloatField(blank=True, null=True)
-    values = models.FloatField(blank=True, null=True)
-    capital = models.FloatField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'stock_price'
-        unique_together = (('date', 'stcd'),)
-
-
 class Stocks(models.Model):
+    """
+    주식의 종목코드 및 종목명
+    """
     stcd = models.CharField(max_length=6)
     stnm = models.TextField(primary_key=True)
     market = models.TextField(blank=True, null=True)
@@ -62,8 +74,11 @@ class Stocks(models.Model):
         db_table = 'stocks'
 
 
-# #
+##
 class CategoryKeywords(models.Model):
+    """
+    워드클라우드
+    """
     date = models.CharField(max_length=12, primary_key=True)
     category_code = models.CharField(max_length=9)
     category_name = models.TextField()
@@ -77,7 +92,52 @@ class CategoryKeywords(models.Model):
         # unique_together = (('date', 'stcd'),)
 
 
+class GainsAndLosers(models.Model):
+    """
+    급등주 및 급락주
+    """
+    date = models.CharField(max_length=8, primary_key=True)
+    stcd = models.CharField(max_length=6)
+    stnm = models.TextField()
+    rate = models.FloatField()
+    open = models.FloatField()
+    high = models.FloatField()
+    low = models.FloatField()
+    close = models.FloatField()
+    volume = models.FloatField()
+
+    class Meta:
+        managed = False
+        db_table = 'cache_gains_and_losers'
+        unique_together = (('date', 'stcd'))
+
+
+class SoaringValue(models.Model):
+    """
+    거래대금 상승종목
+    """
+    date = models.CharField(max_length=8, primary_key=True)
+    stcd = models.CharField(max_length=6)
+    stnm = models.TextField()
+    rate = models.FloatField()
+    open = models.FloatField()
+    high = models.FloatField()
+    low = models.FloatField()
+    close = models.FloatField()
+    volume = models.FloatField()
+    value = models.FloatField()
+
+    class Meta:
+        managed = False
+        db_table = 'cache_gains_and_losers'
+        unique_together = (('date', 'stcd'))
+
+
+##
 class ValuationReturns(models.Model):
+    """
+    기본 valuation의 수익률 결과값
+    """
     name = models.TextField()
     date = models.CharField(max_length=12, primary_key=True)
     return_3m = models.FloatField()
@@ -95,20 +155,3 @@ class ValuationReturns(models.Model):
         managed = False
         db_table = 'valuation_returns'
         # unique_together = (('name', 'date'),)
-
-
-class GainsAndLosers(models.Model):
-    date = models.CharField(max_length=8, primary_key=True)
-    stcd = models.CharField(max_length=6)
-    stnm = models.TextField()
-    rate = models.FloatField()
-    open = models.FloatField()
-    high = models.FloatField()
-    low = models.FloatField()
-    close = models.FloatField()
-    volume = models.FloatField()
-
-    class Meta:
-        managed = False
-        db_table = 'cache_gains_and_losers'
-        unique_together = (('date', 'stcd'))
